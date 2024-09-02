723c1b3dbf154f2aa84d7fae0feea4215e74959a
gutek8134 <gutek8134@gmail.com>
2024-09-02 13:08:55 +0200

    Added dragging functionality

--- scripts/ingredient.gd
+++ scripts/ingredient.gd
@@ -12,6 +12,7 @@ static var time_to_hide_container: float = 0.2
 @onready var container := $Container as SubstanceContainer
 var __mouse_hovering_over := false
 var __mouse_hovering_over_container := false
+var __dragging := false
 var __container_show_timer: SceneTreeTimer
 var __container_hide_timer: SceneTreeTimer
 
@@ -44,11 +45,12 @@ func _on_mouse_entered() -> void:
 
 	__container_show_timer = get_tree().create_timer(time_to_show_container)
 	await __container_show_timer.timeout
-	if __mouse_hovering_over:
+	if __mouse_hovering_over and not __dragging:
 		container.visible = true
 
 func _on_mouse_exited() -> void:
 	__mouse_hovering_over = false
+	__dragging = false
 
 	if not container.visible:
 		if __container_show_timer:
@@ -82,3 +84,18 @@ func _on_container_mouse_exited() -> void:
 	await __container_hide_timer.timeout
 	if not __mouse_hovering_over and not __mouse_hovering_over_container:
 		container.visible = false
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			__dragging = event.pressed
+			if event.pressed:
+				container.visible = false
+			else:
+				__container_show_timer = get_tree().create_timer(time_to_show_container)
+				await __container_show_timer.timeout
+				if __mouse_hovering_over and not __dragging:
+					container.visible = true
+	
+	if __dragging and event is InputEventMouseMotion:
+		position += event.get_screen_relative()
