46c12ac63fe42c41bac6cb566aac21e4794c3ba3
gutek8134 <gutek8134@gmail.com>
2025-03-04 13:40:42 +0100

    Gold displays on the game screen

--- scripts/inventory_manager.gd
+++ scripts/inventory_manager.gd
@@ -7,37 +7,40 @@ const inventory_ui_class = preload("res://scripts/ui/inventory_ui.gd")
 @onready var inventory: Dictionary = {}
 @onready var ingredient_table := IngredientDataTable.factory()
 var inventory_ui: inventory_ui_class
+var gold_display: RichTextLabel
 
-# func _ready() -> void:
-# 	var _a = ((ingredient_table.data["Blue Leaf"] as PackedScene).get_state().get_script() as Ingredient).composition
+func _ready() -> void:
+    gold_display = get_tree().current_scene.get_node("CurrentGold") as RichTextLabel
+    gold_display.text = "[right]Current gold: %d[/right]" % [gold]
+    # var _a = ((ingredient_table.data["Blue Leaf"] as PackedScene).get_state().get_script() as Ingredient).composition
 
 func add_to_inventory(ingredient_name: String, amount: int) -> void:
-	if ingredient_name in inventory:
-		inventory[ingredient_name] += amount
-	else:
-		inventory[ingredient_name] = amount
-	inventory_ui.update()
+    if ingredient_name in inventory:
+        inventory[ingredient_name] += amount
+    else:
+        inventory[ingredient_name] = amount
+    inventory_ui.update()
 
 func remove_from_inventory(ingredient_name: String, amount: int) -> void:
-	if ingredient_name not in inventory:
-		printerr("%s wasn't in inventory" % [ingredient_name])
-		return
-	if inventory[ingredient_name] < amount:
-		printerr("Not enough %s in inventory - %d to be removed, %d in inventory" % [ingredient_name, amount, inventory[ingredient_name]])
-		return
+    if ingredient_name not in inventory:
+        printerr("%s wasn't in inventory" % [ingredient_name])
+        return
+    if inventory[ingredient_name] < amount:
+        printerr("Not enough %s in inventory - %d to be removed, %d in inventory" % [ingredient_name, amount, inventory[ingredient_name]])
+        return
 
-	if inventory[ingredient_name] == amount:
-		inventory.erase(ingredient_name)
-		inventory_ui.update()
-		return
+    if inventory[ingredient_name] == amount:
+        inventory.erase(ingredient_name)
+        inventory_ui.update()
+        return
 
-	inventory[ingredient_name] -= amount
-	inventory_ui.update()
+    inventory[ingredient_name] -= amount
+    inventory_ui.update()
 
 func add_gold(amount: int) -> void:
-	gold += amount
-	print(gold)
+    gold += amount
+    gold_display.text = "[right]Current gold: %d[/right]" % [gold]
 
 func remove_gold(amount: int) -> void:
-	gold -= amount
-	print(gold)
+    gold -= amount
+    gold_display.text = "[right]Current gold: %d[/right]" % [gold]
