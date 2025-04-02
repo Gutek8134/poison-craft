179f701686618c0069009b8939e7d18bcb839a01
gutek8134 <gutek8134@gmail.com>
2025-04-02 09:12:39 +0200

    added distillery to main scene

--- scenes/levels/main_scene.gd
+++ scenes/levels/main_scene.gd
@@ -9,6 +9,7 @@ extends Node2D
 @onready var scene_holder = $Scenes
 @onready var cauldron_scene: Node2D = $Scenes/CauldronScene
 @onready var counter_scene: Node2D = $Scenes/Counter
+@onready var distillery_scene: Node2D = $Scenes/DistilleryScene
 
 @onready var currently_selected_scene: Node2D = cauldron_scene
 
@@ -38,9 +39,21 @@ func _unhandled_key_input(event: InputEvent) -> void:
         scene_movement_timer = get_tree().create_timer(move_time)
         currently_selected_scene = counter_scene
         inventory_ui.update_spawn_node()
+    elif event.is_action_released("Left") and currently_selected_scene == counter_scene:
+        starting_position = scene_holder.position
+        ending_position = scene_holder.position + Vector2.LEFT * move_x
+        scene_movement_timer = get_tree().create_timer(move_time)
+        currently_selected_scene = distillery_scene
+        inventory_ui.update_spawn_node()
     elif event.is_action_released("Right") and currently_selected_scene == counter_scene:
         starting_position = scene_holder.position
         ending_position = scene_holder.position + Vector2.RIGHT * move_x
         scene_movement_timer = get_tree().create_timer(move_time)
         currently_selected_scene = cauldron_scene
+        inventory_ui.update_spawn_node()
+    elif event.is_action_released("Right") and currently_selected_scene == distillery_scene:
+        starting_position = scene_holder.position
+        ending_position = scene_holder.position + Vector2.RIGHT * move_x
+        scene_movement_timer = get_tree().create_timer(move_time)
+        currently_selected_scene = counter_scene
         inventory_ui.update_spawn_node()
