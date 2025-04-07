1971488282de9c967bc8e9030371b2468ff10a2e
gutek8134 <gutek8134@gmail.com>
2025-04-07 08:55:11 +0200

    created rat cage scene

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -10,8 +10,9 @@ extends Node2D
 @onready var cauldron_scene: Node2D = $Scenes/CauldronScene
 @onready var counter_scene: Node2D = $Scenes/Counter
 @onready var distillery_scene: Node2D = $Scenes/DistilleryScene
+@onready var cage_scene: Node2D = $Scenes/RatCage
 
-@onready var scenes = {Vector2i(0, 1): cauldron_scene, Vector2i(1, 1): counter_scene, Vector2i(2, 1): distillery_scene}
+@onready var scenes = {Vector2i(0, 1): cauldron_scene, Vector2i(1, 1): counter_scene, Vector2i(2, 1): distillery_scene, Vector2i(1, 0): cage_scene}
 
 @onready var currently_selected_scene: Node2D = cauldron_scene
 @onready var currently_selected_scene_index: Vector2i = Vector2i(0, 1)
@@ -55,6 +56,28 @@ func _unhandled_key_input(event: InputEvent) -> void:
             return
         starting_position = scene_holder.position
         ending_position = scene_holder.position + Vector2.RIGHT * move_x
+        scene_movement_timer = get_tree().create_timer(move_time)
+        currently_selected_scene = next_scene
+        currently_selected_scene_index = next_scene_index
+        inventory_ui.update_spawn_node()
+    elif event.is_action_released("Up"):
+        next_scene_index = currently_selected_scene_index + Vector2i(0, -1)
+        next_scene = scenes.get(next_scene_index)
+        if next_scene == null:
+            return
+        starting_position = scene_holder.position
+        ending_position = scene_holder.position + Vector2.UP * move_y
+        scene_movement_timer = get_tree().create_timer(move_time)
+        currently_selected_scene = next_scene
+        currently_selected_scene_index = next_scene_index
+        inventory_ui.update_spawn_node()
+    elif event.is_action_released("Down"):
+        next_scene_index = currently_selected_scene_index + Vector2i(0, 1)
+        next_scene = scenes.get(next_scene_index)
+        if next_scene == null:
+            return
+        starting_position = scene_holder.position
+        ending_position = scene_holder.position + Vector2.DOWN * move_y
         scene_movement_timer = get_tree().create_timer(move_time)
         currently_selected_scene = next_scene
         currently_selected_scene_index = next_scene_index
