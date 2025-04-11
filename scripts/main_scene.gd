6440fb4b988136264d8ab30fc74a65ca4371e564
gutek8134 <gutek8134@gmail.com>
2025-04-11 09:49:55 +0200

    Changed direction of switching scenes

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -2,7 +2,7 @@ class_name MainScene
 
 extends Node2D
 
-@export var move_time: float = 2.5
+@export var move_time: float = 1.5
 @export var move_x: float = 1200
 @export var move_y: float = 800
 
@@ -26,59 +26,59 @@ var starting_position: Vector2
 var ending_position: Vector2
 
 func _ready() -> void:
-    inventory_ui.update_spawn_node()
-    InventoryManager.gold_display = $CurrentGold
+	inventory_ui.update_spawn_node()
+	InventoryManager.gold_display = $CurrentGold
 
 func _process(_delta: float) -> void:
-    if scene_movement_timer and is_instance_valid(scene_movement_timer) and not is_zero_approx(scene_movement_timer.time_left):
-        scene_holder.position = starting_position.lerp(ending_position, (move_time - scene_movement_timer.time_left) / move_time)
+	if scene_movement_timer and is_instance_valid(scene_movement_timer) and not is_zero_approx(scene_movement_timer.time_left):
+		scene_holder.position = starting_position.lerp(ending_position, (move_time - scene_movement_timer.time_left) / move_time)
 
 func _unhandled_key_input(event: InputEvent) -> void:
-    if scene_movement_timer and is_instance_valid(scene_movement_timer) and not is_zero_approx(scene_movement_timer.time_left):
-        return
-    var next_scene: Node2D
-    var next_scene_index: Vector2i
-    if event.is_action_released("Left"):
-        next_scene_index = currently_selected_scene_index + Vector2i(1, 0)
-        next_scene = scenes.get(next_scene_index)
-        if next_scene == null:
-            return
-        starting_position = scene_holder.position
-        ending_position = scene_holder.position + Vector2.LEFT * move_x
-        scene_movement_timer = get_tree().create_timer(move_time)
-        currently_selected_scene = next_scene
-        currently_selected_scene_index = next_scene_index
-        inventory_ui.update_spawn_node()
-    elif event.is_action_released("Right"):
-        next_scene_index = currently_selected_scene_index + Vector2i(-1, 0)
-        next_scene = scenes.get(next_scene_index)
-        if next_scene == null:
-            return
-        starting_position = scene_holder.position
-        ending_position = scene_holder.position + Vector2.RIGHT * move_x
-        scene_movement_timer = get_tree().create_timer(move_time)
-        currently_selected_scene = next_scene
-        currently_selected_scene_index = next_scene_index
-        inventory_ui.update_spawn_node()
-    elif event.is_action_released("Up"):
-        next_scene_index = currently_selected_scene_index + Vector2i(0, -1)
-        next_scene = scenes.get(next_scene_index)
-        if next_scene == null:
-            return
-        starting_position = scene_holder.position
-        ending_position = scene_holder.position + Vector2.UP * move_y
-        scene_movement_timer = get_tree().create_timer(move_time)
-        currently_selected_scene = next_scene
-        currently_selected_scene_index = next_scene_index
-        inventory_ui.update_spawn_node()
-    elif event.is_action_released("Down"):
-        next_scene_index = currently_selected_scene_index + Vector2i(0, 1)
-        next_scene = scenes.get(next_scene_index)
-        if next_scene == null:
-            return
-        starting_position = scene_holder.position
-        ending_position = scene_holder.position + Vector2.DOWN * move_y
-        scene_movement_timer = get_tree().create_timer(move_time)
-        currently_selected_scene = next_scene
-        currently_selected_scene_index = next_scene_index
-        inventory_ui.update_spawn_node()
+	if scene_movement_timer and is_instance_valid(scene_movement_timer) and not is_zero_approx(scene_movement_timer.time_left):
+		return
+	var next_scene: Node2D
+	var next_scene_index: Vector2i
+	if event.is_action_released("Left"):
+		next_scene_index = currently_selected_scene_index + Vector2i(-1, 0)
+		next_scene = scenes.get(next_scene_index)
+		if next_scene == null:
+			return
+		starting_position = scene_holder.position
+		ending_position = scene_holder.position + Vector2.RIGHT * move_x
+		scene_movement_timer = get_tree().create_timer(move_time)
+		currently_selected_scene = next_scene
+		currently_selected_scene_index = next_scene_index
+		inventory_ui.update_spawn_node()
+	elif event.is_action_released("Right"):
+		next_scene_index = currently_selected_scene_index + Vector2i(1, 0)
+		next_scene = scenes.get(next_scene_index)
+		if next_scene == null:
+			return
+		starting_position = scene_holder.position
+		ending_position = scene_holder.position + Vector2.LEFT * move_x
+		scene_movement_timer = get_tree().create_timer(move_time)
+		currently_selected_scene = next_scene
+		currently_selected_scene_index = next_scene_index
+		inventory_ui.update_spawn_node()
+	elif event.is_action_released("Up"):
+		next_scene_index = currently_selected_scene_index + Vector2i(0, -1)
+		next_scene = scenes.get(next_scene_index)
+		if next_scene == null:
+			return
+		starting_position = scene_holder.position
+		ending_position = scene_holder.position + Vector2.DOWN * move_y
+		scene_movement_timer = get_tree().create_timer(move_time)
+		currently_selected_scene = next_scene
+		currently_selected_scene_index = next_scene_index
+		inventory_ui.update_spawn_node()
+	elif event.is_action_released("Down"):
+		next_scene_index = currently_selected_scene_index + Vector2i(0, 1)
+		next_scene = scenes.get(next_scene_index)
+		if next_scene == null:
+			return
+		starting_position = scene_holder.position
+		ending_position = scene_holder.position + Vector2.UP * move_y
+		scene_movement_timer = get_tree().create_timer(move_time)
+		currently_selected_scene = next_scene
+		currently_selected_scene_index = next_scene_index
+		inventory_ui.update_spawn_node()
